e8053383a00149bf28e6e0ccf9d6ce19e1a18e67
Bruno Strano <bruno_strano@hotmail.com>
2023-08-08 22:57:39 -0300

    🔧 Cors config

--- Keychain.API/Program.cs
+++ Keychain.API/Program.cs
@@ -1,11 +1,24 @@
 using Keychain_API;
 using Keychain.Application;
 using Keychain.Infrastructure;
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddPresentation();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://*.stralom.com")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 
 var app = builder.Build();
@@ -19,6 +32,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
